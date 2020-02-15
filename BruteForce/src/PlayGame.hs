7a82c7c96fd998f70aeab9e7f7272c1e354dea0a
wasd <wasdsweden@gmail.com>
2020-02-16 00:21:55 +0100

    can now play against bot, but it is not working properly

--- BruteForce/src/PlayGame.hs
+++ BruteForce/src/PlayGame.hs
@@ -6,6 +6,9 @@ import Bot
 import Board
 import ListBoard
 
+playAgainstBot :: Bool
+playAgainstBot = True
+
 playGame :: IO ()
 playGame = do
   gameOver <- playUntilDone P1 (emptyBoard :: ListBoard)
@@ -15,7 +18,9 @@ playGame = do
 
 playUntilDone :: Board b => Player -> b -> IO GameOver
 playUntilDone player board = do
-  (board', maybeGameOver) <- oneMove player board
+  (board', maybeGameOver) <- if playAgainstBot && player == P2
+                               then botMove player board
+                               else oneMove player board
   case maybeGameOver of
     Just gameOver -> print board' >> return gameOver
     Nothing -> playUntilDone (nextPlayer player) board'
@@ -33,6 +38,14 @@ oneMove player board = do
                        in  return (board', getWinner board')
                        
                   else putStrLn "Illegal move!" >> oneMove player board
+
+-- FIXME: player (1,1), bot (0,0), player (1,2), bot (0,1), player (1,0), player win!
+botMove :: Board b => Player -> b -> IO (b, Maybe GameOver)
+botMove player board = do
+  let pos = selectMove player board
+  let board' = makeMove player pos board
+  putStrLn $ "Bot places " ++ show player ++ " on... " ++ show pos
+  return (board', getWinner board')
 
 parsePos :: String -> Maybe Position
 parsePos [x,' ',y] = if isDigit x && isDigit y
